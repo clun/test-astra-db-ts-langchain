cdb072c0b844d9e7fb075675c81d735df816ba07
Cedrick Lunven <cedrick.lunven@gmail.com>
2024-02-26 16:50:48 +0100

    reproduce error

--- src/demo-errconnect.ts
+++ src/demo-errconnect.ts
@@ -1,8 +1,10 @@
 import * as fs from 'fs';
 import * as util from 'util';
+import { v4 as uuid } from 'uuid';
 
 import { AstraDB, Collection } from "@datastax/astra-db-ts";
 import { OpenAI } from "openai";
+import { OpenAIEmbeddings } from "@langchain/openai";
 
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { TextLoader } from "langchain/document_loaders/fs/text";
@@ -29,45 +31,63 @@ function chunkArray<T>(array: T[], chunkSize: number): T[][] {
   return chunks;
 }
 
+type AstraDoc = {
+    fileId: string;
+    text: string;
+    $vector: number[];
+};
+
 async function main() {
 
     try {
-        const loader = new DirectoryLoader(FILE_PATH, { ".txt": path => new TextLoader(path) });
-        const docs = await loader.load();
-    
-        const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 15 });
-        const splitStrings = await splitter.splitDocuments(docs);
-        
-        // Prepare the requests for embedding
-        const embeddingPromises = splitStrings.map(async (text) => {
-            return openai.embeddings({
-            model: "text-embedding-ada-002",
-            input: text,
-            });
+        const loader       = new DirectoryLoader(FILE_PATH, { ".txt": path => new TextLoader(path) });
+        const docs         = await loader.load();
+        const splitter     = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 15 });
+        const splitDocs    = await splitter.splitDocuments(docs);
+        const splitStrings = splitDocs.map((doc) => doc.pageContent);
+
+        const embeddings = new OpenAIEmbeddings({ 
+            openAIApiKey: process.env.OPENAI_API_KEY, 
+            batchSize: 512,
+            modelName: "text-embedding-3-small",
+            maxConcurrency: 10,
+            // verbose: true,
         });
 
-        // Log the start of embedding
-        console.log("Starting Embedding");
-
-        // Compute embeddings for each split string
+        //console.log("Starting Embedding");
         const documentEmbeddings = await embeddings.embedDocuments(splitStrings);
+        //console.log("Document embeddings:", JSON.stringify(documentEmbeddings));
+
+        const astraDocs: AstraDoc[] = splitStrings.map((splitString, index) => ({
+          fileId: uuid(),
+          text: splitString,
+          $vector: documentEmbeddings[index],
+        }));
 
         console.log("Inserting into Astra");
 
         // Initialize the database connection
         const db = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_API_ENDPOINT);
-        const collection: Collection = await db.createCollection('collection_vector1', {
+        const collection: Collection = await db.createCollection('collection_vector', {
             "vector": {
                 "dimension": 1536,
                 "metric": "cosine"
             }
         });
+
+        console.log("Document embeddings: " + astraDocs.length);
+        console.log("Document embeddings: " + splitDocs.length);
         
+        const collection2 : Collection = await db.collection("collection_vector");
         // Create batches of the `astraDocs`, here assuming `20` as the batch size
-        const batches = chunkArray(splitDocs, 20);
-        
+        //const batches = chunkArray(splitDocs, 20);
+        const batches = chunkArray(astraDocs, 20);
+
         // Map each batch to an insertMany operation, creating an array of Promises
-        const batchesReq = batches.map((batch) => collection.insertMany(batch));
+        const batchesReq = batches.map((batch) => {
+            collection2.insertMany(batch);
+        }
+        );
         
         // Use Promise.all to wait for all insertMany Promises to resolve
         const res = await Promise.all(batchesReq);
